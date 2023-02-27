b146043585b02204426d9d91e119320782a6e1b4
Joris Vandenberg <joris.vandenberg@scarlet.be>
2023-02-27 09:56:02 -0500

    made the sql statement load function more logical

--- querydb.go
+++ querydb.go
@@ -18,172 +18,31 @@ var myquerydb = make(map[string]querydb)
 func loadquerydb(tx *sql.Tx) {
 	var myquery querydb
 
-	//statement to insert a new user
-	query_insertuser := "insert into user(ip, useragent) values (?,?)"
-	stmt_insertuser, err := tx.Prepare(query_insertuser)
-	if err != nil {
-		fmt.Printf("%s\n", err.Error())
-		os.Exit(1)
+	querymap := make(map[string]string)
+	querymap["stmt_insertuser"] = "insert into user(ip, useragent) values (?,?)" //insert a new user
+	querymap["stmt_insertrequest"] = "insert into request(request) values (?)" //insert a new request
+	querymap["stmt_countreferrer"] = "select count(*) from referrer where referrer = ?" //count wether or not a certain referrer already exists
+	querymap["stmt_selectreferrerid"] = "select id from referrer where referrer = ?" //return the id of a unique referrer
+	querymap["stmt_insertreferrer"] = "select id from referrer where referrer = ?" //insert a new referrer
+	querymap["stmt_selectrequestid"] = "select id from request where request = ?" //return the id of a unique request
+	querymap["stmt_countrequest"] = "select count(*) from request where request = ?" //count wether or not a certain request already exists
+	querymap["stmt_countusers"] = "select count(*) from user where ip = ? and useragent = ?" //count wether or not a certain user already exists
+	querymap["stmt_selectuserid"] = "select id from user where ip = ? and useragent = ?" //return the id of a unique user
+	querymap["stmt_insertvisit"] = "insert into visit(referrer, request,  visit_timestamp, user, statuscode, httpsize) values (?,?,?,?,?,?)" //insert a new visit record into the database
+	querymap["stmt_countalreadyloaded"] = "select count(*) from alreadyloaded where hash = ?" //count wether or not a file was already succesfully parsed in the past
+	querymap["stmt_insertalreadyloaded"] = "insert into alreadyloaded(hash) values (?)" //insert a new sucesfully loaded file's hash into the database
+	querymap["stmt_truncatealreadyloaded"] = "DELETE FROM alreadyloaded" //truncate the alreadyloaded table so the system doesn't know wether a file was already loaded in the past
+	querymap["stmt_maxvisittimestamp"] = "select max(visit_timestamp) from visit" //select the latest succesfully added record's timestamp to skip older records when loading
+	for naam, sql := range querymap {
+		stmt, err := tx.Prepare(sql)
+		if err != nil {
+			fmt.Printf("%s\n", err.Error())
+			os.Exit(1)
+		}
+		myquery.querynaam = naam
+		myquery.sqlcode = sql
+		myquery.stmt = stmt
+		myquerydb[naam] = myquery
 	}
-	myquery.querynaam = "stmt_insertuser"
-	myquery.sqlcode = query_insertuser
-	myquery.stmt = stmt_insertuser
-	myquerydb["stmt_insertuser"] = myquery
-
-	//query to insert a new request
-	query_insertrequest := "insert into request(request) values (?)"
-	stmt_insertrequest, err := tx.Prepare(query_insertrequest)
-	if err != nil {
-		fmt.Printf("%s\n", err.Error())
-		os.Exit(1)
-	}
-	myquery.querynaam = "stmt_insertrequest"
-	myquery.sqlcode = query_insertrequest
-	myquery.stmt = stmt_insertrequest
-	myquerydb["stmt_insertrequest"] = myquery
-
-	//statement to count if a referrer already exists
-	query_countreferrer := "select count(*) from referrer where referrer = ?"
-	stmt_countreferrer, err := tx.Prepare(query_countreferrer)
-	if err != nil {
-		fmt.Printf("%s\n", err.Error())
-		os.Exit(1)
-	}
-	myquery.querynaam = "stmt_countreferrer"
-	myquery.sqlcode = query_countreferrer
-	myquery.stmt = stmt_countreferrer
-	myquerydb["stmt_countreferrer"] = myquery
-
-	//statement to get the id of an existing referrer
-	query_selectreferrerid := "select id from referrer where referrer = ?"
-	stmt_selectreferrerid, err := tx.Prepare(query_selectreferrerid)
-	if err != nil {
-		fmt.Printf("%s\n", err.Error())
-		os.Exit(1)
-	}
-	myquery.querynaam = "stmt_selectreferrerid"
-	myquery.sqlcode = query_selectreferrerid
-	myquery.stmt = stmt_selectreferrerid
-	myquerydb["stmt_selectreferrerid"] = myquery
-
-	//statement to insert a new referrer
-	query_insertreferrer := "insert into referrer(referrer) values (?)"
-	stmt_insertreferrer, err := tx.Prepare(query_insertreferrer)
-	if err != nil {
-		fmt.Printf("%s\n", err.Error())
-		os.Exit(1)
-	}
-	myquery.querynaam = "stmt_insertreferrer"
-	myquery.sqlcode = query_insertreferrer
-	myquery.stmt = stmt_insertreferrer
-	myquerydb["stmt_insertreferrer"] = myquery
-
-	//statement to get the id of a unique statement
-	query_selectrequestid := "select id from request where request = ?"
-	stmt_selectrequestid, err := tx.Prepare(query_selectrequestid)
-	if err != nil {
-		fmt.Printf("%s\n", err.Error())
-		os.Exit(1)
-	}
-	myquery.querynaam = "stmt_selectrequestid"
-	myquery.sqlcode = query_selectrequestid
-	myquery.stmt = stmt_selectrequestid
-	myquerydb["stmt_selectrequestid"] = myquery
-
-	//statement to count if a request already exists
-	query_countrequest := "select count(*) from request where request = ?"
-	stmt_countrequest, err := tx.Prepare(query_countrequest)
-	if err != nil {
-		fmt.Printf("%s\n", err.Error())
-		os.Exit(1)
-	}
-	myquery.querynaam = "stmt_countrequest"
-	myquery.sqlcode = query_countrequest
-	myquery.stmt = stmt_countrequest
-	myquerydb["stmt_countrequest"] = myquery
-
-	//statement to count unique users 
-	query_countusers := "select count(*) from user where ip = ? and useragent = ?"
-	stmt_countusers, err := tx.Prepare(query_countusers)
-	if err != nil {
-		fmt.Printf("%s\n", err.Error())
-		os.Exit(1)
-	}
-	myquery.querynaam = "stmt_countusers"
-	myquery.sqlcode = query_countusers
-	myquery.stmt = stmt_countusers
-	myquerydb["stmt_countusers"] = myquery
-
-	//statement to get the id of an existing user record
-	query_selectuserid := "select id from user where ip = ? and useragent = ?"
-	stmt_selectuserid, err := tx.Prepare(query_selectuserid)
-	if err != nil {
-		fmt.Printf("%s\n", err.Error())
-		os.Exit(1)
-	}
-	myquery.querynaam = "stmt_selectuserid"
-	myquery.sqlcode = query_selectuserid
-	myquery.stmt = stmt_selectuserid
-	myquerydb["stmt_selectuserid"] = myquery
-
-	//statement for inserting new visit 
-	query_insertvisit := "insert into visit(referrer, request,  visit_timestamp, user, statuscode, httpsize) values (?,?,?,?,?,?)"
-	stmt_insertvisit, err := tx.Prepare(query_insertvisit)
-	if err != nil {
-		fmt.Printf("%s\n", err.Error())
-		os.Exit(1)
-	}
-	myquery.querynaam = "stmt_insertvisit"
-	myquery.sqlcode = query_insertvisit
-	myquery.stmt = stmt_insertvisit
-	myquerydb["stmt_insertvisit"] = myquery
-
-	//statement to count if a file was loaded previously
-	query_countalreadyloaded := "select count(*) from alreadyloaded where hash = ?"
-	stmt_countalreadyloaded, err := tx.Prepare(query_countalreadyloaded)
-	if err != nil {
-		fmt.Printf("%s\n", err.Error())
-		os.Exit(1)
-	}
-	myquery.querynaam = "stmt_countalreadyloaded"
-	myquery.sqlcode = query_countalreadyloaded
-	myquery.stmt = stmt_countalreadyloaded
-	myquerydb["stmt_countalreadyloaded"] = myquery
-
-	//statement to insert the hash of a succesfully loaded file
-	query_insertalreadyloaded := "insert into alreadyloaded(hash) values (?)"
-	stmt_insertalreadyloaded, err := tx.Prepare(query_insertalreadyloaded)
-	if err != nil {
-		fmt.Printf("%s\n", err.Error())
-		os.Exit(1)
-	}
-	myquery.querynaam = "stmt_insertalreadyloaded"
-	myquery.sqlcode = query_insertalreadyloaded
-	myquery.stmt = stmt_insertalreadyloaded
-	myquerydb["stmt_insertalreadyloaded"] = myquery
-
-	//debug statement to clean up de alreadyloaded table
-	query_truncatealreadyloaded := "DELETE FROM alreadyloaded"
-	stmt_truncatealreadyloaded, err := tx.Prepare(query_truncatealreadyloaded)
-	if err != nil {
-		fmt.Printf("%s\n", err.Error())
-		os.Exit(1)
-	}
-	myquery.querynaam = "stmt_truncatealreadyloaded"
-	myquery.sqlcode = query_truncatealreadyloaded
-	myquery.stmt = stmt_truncatealreadyloaded
-	myquerydb["stmt_truncatealreadyloaded"] = myquery
-
-	//statement for fetching the last known timestamp from the db
-	query_maxvisittimestamp := "select max(visit_timestamp) from visit"
-	stmt_maxvisittimestamp, err := tx.Prepare(query_maxvisittimestamp)
-	if err != nil {
-		fmt.Printf("%s\n", err.Error())
-		os.Exit(1)
-	}
-	myquery.querynaam = "stmt_maxvisittimestamp"
-	myquery.sqlcode = query_maxvisittimestamp
-	myquery.stmt = stmt_maxvisittimestamp
-	myquerydb["stmt_maxvisittimestamp"] = myquery
 
 }
