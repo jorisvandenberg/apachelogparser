daa24b1106ae07f289f0f9432068dfe523a62b5b
Joris Vandenberg <joris.vandenberg@scarlet.be>
2023-03-03 03:02:41 -0500

    forgot to save the new rendering method for linegraph charts

--- charts_linegraph.go
+++ charts_linegraph.go
@@ -6,58 +6,6 @@ import (
 	"os"
 )
 
-/*
-
-func createlinegraph_html(XValues []string, YValues map[string][]int, title string, subtitle string, args args, filename string, writehtml ...bool) *charts.Line {
-	line := charts.NewLine()
-	line.Renderer = newSnippetRenderer(line, line.Validate)
-	line.SetGlobalOptions(
-		charts.WithTitleOpts(opts.Title{
-			Title:    title,
-			Subtitle: subtitle,
-			//Link:     "https://github.com/go-echarts/go-echarts",
-		}),
-	)
-	for serienaam, serievalues := range YValues {
-		items := make([]opts.LineData, 0)
-		for _, serievalue := range serievalues {
-			items = append(items, opts.LineData{Value: serievalue})
-		}
-		line.SetXAxis(XValues).AddSeries(serienaam, items).SetSeriesOptions(charts.WithLabelOpts(opts.Label{Show: true}))
-	}
-
-	line.SetGlobalOptions(
-		charts.WithInitializationOpts(opts.Initialization{
-			PageTitle:  title,
-			Width:      `95vw`,
-			Height:     `95vh`,
-			AssetsHost: args.assethost,
-		}),
-		charts.WithLegendOpts(opts.Legend{
-			Show:  true,
-			Top:   "10%",
-			Right: "5%",
-		}),
-	)
-
-	writehtml_optional := true
-	if len(writehtml) > 0 {
-		writehtml_optional = writehtml[0]
-	}
-	if writehtml_optional {
-		f, _ := os.Create(args.outputpath + filename)
-		_ = line.Render(f)
-		MyPageForIndex := page_forindex{
-			Title: title,
-			Url:   filename,
-		}
-		indexpages = append(indexpages, MyPageForIndex)
-	}
-	return line
-
-}
-*/
-
 func createlinegraph(XValues []string, YValues map[string][]int, title string, subtitle string, args args, filename string, writehtml ...bool) *charts.Line {
 	line := charts.NewLine()
 	line.Renderer = newSnippetRenderer(line, line.Validate)
