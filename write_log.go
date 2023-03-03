4add2642759b9e9e6051c9d0f1852433b51c0a46
Joris Vandenberg <joris.vandenberg@scarlet.be>
2023-03-03 09:07:16 -0500

    logs link was duplicated due to having it in both functions

--- write_log.go
+++ write_log.go
@@ -4,15 +4,9 @@ func writelog(args args) {
 	//var mylog []string
 	if args.writelog {
 		var newpage HtmlPage
-		newpage.Pagetitle = "logggggs"
+		newpage.Pagetitle = "detailed runlogs of the statistics application"
 		newpage.Pagedescription = "here are the logs :)"
 		newpage.Paragraphs = mylog
 		createhtmltable(args, "logs.html", newpage)
-
-		MyPageForIndex := page_forindex{
-			Title: "detailed runlogs of the statistics application",
-			Url:   "logs.html",
-		}
-		indexpages = append(indexpages, MyPageForIndex)
 	}
 }
