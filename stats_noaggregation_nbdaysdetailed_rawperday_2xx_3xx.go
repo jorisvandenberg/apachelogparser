7ba436b973539fe29848f9363e07851fa2b70f4c
Joris Vandenberg <joris.vandenberg@scarlet.be>
2023-03-08 08:16:51 -0500

    raw hits per day now configured in main configfile
    instead of hardcoded

--- stats_noaggregation_nbdaysdetailed_rawperday_2xx_3xx.go
+++ stats_noaggregation_nbdaysdetailed_rawperday_2xx_3xx.go
@@ -2,56 +2,77 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"strconv"
 	"time"
 )
 
 func noaggregation_nbdaysdetailed_rawperday_2xx_3xx(args Args) {
-	logger("i'm going to generate a table and a linechart with the dayly raw hits")
-	stmt_raw_2xx_3xx_daily_maxnbofdaysdetailed := myquerydb["stmt_raw_2xx_3xx_daily_maxnbofdaysdetailed"].stmt
-	mintimestamp := int(time.Now().Unix()) - (args.Outputs.Number_of_days_detailed * 86400)
-	rows, err := stmt_raw_2xx_3xx_daily_maxnbofdaysdetailed.Query(mintimestamp)
-	if err != nil {
-		fmt.Printf("%s\n", err.Error())
-	}
-	defer rows.Close()
+	check_if_stats_is_slice := reflect.ValueOf(args).FieldByName("Stats")
+	foundcurstat := false
+	var mycurstat Statconfig
+	if check_if_stats_is_slice.Kind() == reflect.Ptr && check_if_stats_is_slice.IsNil() {
+		logger("i wanted to verify if i had to create stats with the daily raw hits, but it seems like all Stats are disabled in the config")
 
-	MyHeaders := map[string]string{
-		"Title_1": "YEAR",
-		"Title_2": "MONTH",
-		"Title_3": "DAY",
-		"Title_5": "NB RAW HITS",
+	} else if check_if_stats_is_slice.Kind() == reflect.Slice {
+		for _, curstat := range args.Stats {
+			if (curstat.Statname == "stat_perday_hits_raw_2xx_3xx") {
+			foundcurstat = true
+			mycurstat = curstat
+			}
+			
+		}
 	}
-	myTable := Table{
-		Pagetitle:       "Number of raw 2xx and 3xx hits per day over th last " + strconv.Itoa(args.Outputs.Number_of_days_detailed) + " days",
-		Pagedescription: "Count of all raw succesfull hits (filtering out all 4xx and 5xx return codes).",
-		Pagecontent:     []string{"We limit the output to the number of days that were defined in your config.ini file with a sliding window (so if you run this tool at 15:34 you'll get stats untill 15:34 x days ago)."},
-		Pagefooter:      "only hits that were actually loaded are shown, so if you filtered out certain lines in your config.ini they'll never be shown!",
-		Headers:         MyHeaders,
-		Data:            []map[string]string{},
-	}
-
-	var XValues_linegraph []string
-	YValues_linegraph := make(map[string][]int)
-	for rows.Next() {
-		var year, month, day, count int
-		if err := rows.Scan(&year, &month, &day, &count); err != nil {
+	if foundcurstat {
+		logger("i'm going to generate a table and/or a linechart (depending on the config) with the daily raw hits")
+		stmt_raw_2xx_3xx_daily_maxnbofdaysdetailed := myquerydb["stmt_raw_2xx_3xx_daily_maxnbofdaysdetailed"].stmt
+		mintimestamp := int(time.Now().Unix()) - (args.Outputs.Number_of_days_detailed * 86400)
+		rows, err := stmt_raw_2xx_3xx_daily_maxnbofdaysdetailed.Query(mintimestamp)
+		if err != nil {
 			fmt.Printf("%s\n", err.Error())
 		}
-		MyData := map[string]string{
-			"Value_1": strconv.Itoa(year),
-			"Value_2": strconv.Itoa(month),
-			"Value_3": strconv.Itoa(day),
-			"Value_5": strconv.Itoa(count),
-		}
-		myTable.Data = append(myTable.Data, MyData)
-		XValues_linegraph = append(XValues_linegraph, strconv.Itoa(year)+"-"+strconv.Itoa(month)+"-"+strconv.Itoa(day))
-		YValues_linegraph["raw hits"] = append(YValues_linegraph["raw hits"], count)
-	}
+		defer rows.Close()
 
-	createtable(args, "noaggregation_nbdaysdetailed_rawperday_2xx_3xx_table.html", "table of the raw 2xx and 3xx per day over the last "+strconv.Itoa(args.Outputs.Number_of_days_detailed)+" days", myTable, "hits", 3)
-	PreChartText = ""
-	PostChartText = ""
-	createlinegraph(XValues_linegraph, YValues_linegraph, "line graph of the raw hits with status 2xx and 3xx", "Count of all raw succesfull hits (filtering out all 4xx and 5xx return codes).", args, "noaggregation_nbdaysdetailed_rawperday_2xx_3xx_linegraph.html", "hits", 3)
-	logger("finished generating a table and a linechart with the dayly raw hits")
+		MyHeaders := map[string]string{
+			"Title_1": "YEAR",
+			"Title_2": "MONTH",
+			"Title_3": "DAY",
+			"Title_5": "NB RAW HITS",
+		}
+		myTable := Table{
+			Pagetitle:       mycurstat.Tableinfo.Table_title,
+			Pagedescription: mycurstat.Tableinfo.Table_description,
+			Pagecontent:     mycurstat.Tableinfo.Table_pagecontent,
+			Pagefooter:      mycurstat.Tableinfo.Table_pagefooter,
+			Headers:         MyHeaders,
+			Data:            []map[string]string{},
+		}
+
+		var XValues_linegraph []string
+		YValues_linegraph := make(map[string][]int)
+		for rows.Next() {
+			var year, month, day, count int
+			if err := rows.Scan(&year, &month, &day, &count); err != nil {
+				fmt.Printf("%s\n", err.Error())
+			}
+			MyData := map[string]string{
+				"Value_1": strconv.Itoa(year),
+				"Value_2": strconv.Itoa(month),
+				"Value_3": strconv.Itoa(day),
+				"Value_5": strconv.Itoa(count),
+			}
+			myTable.Data = append(myTable.Data, MyData)
+			XValues_linegraph = append(XValues_linegraph, strconv.Itoa(year)+"-"+strconv.Itoa(month)+"-"+strconv.Itoa(day))
+			YValues_linegraph["raw hits"] = append(YValues_linegraph["raw hits"], count)
+		}
+		if mycurstat.Tableinfo.Table_enabled {
+			createtable(args, mycurstat.Tableinfo.Table_filename, mycurstat.Tableinfo.Table_index_name, myTable, mycurstat.Tableinfo.Table_index_group, mycurstat.Tableinfo.Table_index_order)
+		}
+		if mycurstat.Linegraphinfo.Linegraph_enabled {
+			createlinegraph(XValues_linegraph, YValues_linegraph, mycurstat.Linegraphinfo.Linegraph_title, mycurstat.Linegraphinfo.Linegraph_description, args, mycurstat.Linegraphinfo.Linegraph_filename, mycurstat.Linegraphinfo.Linegraph_index_group, mycurstat.Linegraphinfo.Linegraph_index_order)
+		}
+		logger("finished generating a table and/or a linechart with the daily raw hits")
+	} else {
+		logger("i could not find this stat stat_perday_hits_raw_2xx_3xx in the config. Is it disabled?")
+	}
 }
