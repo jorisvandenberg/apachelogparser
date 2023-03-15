e7c5b376583c9b851236545666f3bf6b7eb9119a
Joris Vandenberg <joris.vandenberg@scarlet.be>
2023-03-15 09:29:59 -0400

    raw and unique 2xx and 3xx now going trough new function

--- stats_generate.go
+++ stats_generate.go
@@ -1,11 +1,48 @@
 package main
 
+import (
+	"strconv"
+	"time"
+	)
+	
 func generatestats(args Args) {
 	logger("started the function to generate statistics")
+	
+	mintimestamp := int(time.Now().Unix()) - (args.Outputs.Number_of_days_detailed * 86400)
+	
+	/*
+	stat: unique 2xx and 3xx hits over the last 31 (default) days
+	expecting 3 htmls: 
+	noaggregation_nbdaysdetailed_uniqueperday_2xx_3xx_table.html
+	noaggregation_nbdaysdetailed_uniqueperday_2xx_3xx_linegraph.html
+	noaggregation_nbdaysdetailed_unique_2xx_3xx_linegraph_4weekcompare.html
+	*/
+	parameters := []interface{}{mintimestamp}
+	tableheaders := map[string]string{
+		"Title_1": "YEAR",
+		"Title_2": "MONTH",
+		"Title_3": "DAY",
+		"Title_4": "NB RAW HITS",
+	}
+	xaxisfields := []int{0,1,2}
+	valuefield := 3
+	genstats(args, "valid unique hits per day over the last " + strconv.Itoa(args.Outputs.Number_of_days_detailed) + " days" , "stat_perday_hits_unique_2xx_3xx", "stmt_unique_2xx_3xx_dayly_maxnbofdaysdetailed", parameters, tableheaders,xaxisfields, valuefield, "raw hits" )
+	
+	/*
+	stat: raw 2xx and 3xx hits over the last 31 (default) days
+	expecting 3 htmls: 
+	noaggregation_nbdaysdetailed_rawperday_2xx_3xx_table.html
+	noaggregation_nbdaysdetailed_rawperday_2xx_3xx_linegraph.html
+	noaggregation_nbdaysdetailed_raw_2xx_3xx_linegraph_4weekcompare.html
+	*/
+	//identical parameters, tableheaders, xaxisfields, valuefield as the stat above... not re-initialising!
+	genstats(args, "valid raw hits per day over the last " + strconv.Itoa(args.Outputs.Number_of_days_detailed) + " days" , "stat_perday_hits_raw_2xx_3xx", "stmt_raw_2xx_3xx_daily_maxnbofdaysdetailed", parameters, tableheaders,xaxisfields, valuefield, "unique hits" )
+	
+	
 	noaggregation_nbdaysdetailed_raw_2xx_3xx(args)
 	noaggregation_nbdaysdetailed_unique_2xx_3xx(args)
-	noaggregation_nbdaysdetailed_rawperday_2xx_3xx(args)
-	noaggregation_nbdaysdetailed_uniqueperday_2xx_3xx(args)
+	//noaggregation_nbdaysdetailed_rawperday_2xx_3xx(args)
+	//noaggregation_nbdaysdetailed_uniqueperday_2xx_3xx(args)
 	stats_noaggregation_nbdaysdetailed_refferers(args)
 	stats_noaggregation_nbdaysdetailed_unique_refferers(args)
 	stats_noaggregation_nbdaysdetailed_unique_refferers_noemptyorself(args)
