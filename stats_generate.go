5dd5d0b528e8b60128acb2b59f0f57e4adc89109
Joris Vandenberg <joris.vandenberg@scarlet.be>
2023-03-15 10:06:55 -0400

    sum of unique hits per referrer over the last 31 (default) days

--- stats_generate.go
+++ stats_generate.go
@@ -79,12 +79,43 @@ func generatestats(args Args) {
 	genstats(args, "valid raw hits per hour over the last " + strconv.Itoa(args.Outputs.Number_of_days_detailed) + " days" , "stat_perhour_hits_raw_2xx_3xx", "stmt_raw_2xx_3xx_hourly_maxnbofdaysdetailed", parameters, tableheaders,xaxisfields, valuefield, "raw hits" )
 	
 	
+	/*
+	stat: sum of raw hit per referrer over the last 31 (default) days
+	expecting 1 htmls: 
+	noaggregation_nbdaysdetailed_rawreferrers_2xx_3xx_table.html
+	*/
+	parameters = []interface{}{mintimestamp, int(args.Outputs.Numberofreferrers)}
+	tableheaders = map[string]string{
+		"Title_1": "REFERRER",
+		"Title_2": "NB of raw hits",
+			
+	}
+	xaxisfields = []int{0}
+	valuefield = 1
+	genstats(args, "sum of raw hits per referrer over last " + strconv.Itoa(args.Outputs.Number_of_days_detailed) + " days" , "stat_perhour_referrers_raw_2xx_3xx", "stmt_noaggregation_nbdaysdetailed_refferers_noparams_2xx_3xx", parameters, tableheaders,xaxisfields, valuefield, "" )
+	
+	
+	/*
+	stat: sum of unique hits per referrer over the last 31 (default) days
+	expecting 1 htmls: 
+	noaggregation_nbdaysdetailed_uniquereferrers_2xx_3xx_table.html
+	*/
+	parameters = []interface{}{mintimestamp, int(args.Outputs.Numberofreferrers)}
+	tableheaders = map[string]string{
+		"Title_1": "REFERRER",
+		"Title_2": "NB of unique hits",
+			
+	}
+	xaxisfields = []int{0}
+	valuefield = 1
+	genstats(args, "sum of unique hits per referrer over last " + strconv.Itoa(args.Outputs.Number_of_days_detailed) + " days" , "stat_perhour_referrers_unique_2xx_3xx", "stmt_noaggregation_nbdaysdetailed_unique_refferers_noparams_2xx_3xx", parameters, tableheaders,xaxisfields, valuefield, "" )
+	
 	//noaggregation_nbdaysdetailed_raw_2xx_3xx(args)
 	//noaggregation_nbdaysdetailed_unique_2xx_3xx(args)
 	//noaggregation_nbdaysdetailed_rawperday_2xx_3xx(args)
 	//noaggregation_nbdaysdetailed_uniqueperday_2xx_3xx(args)
-	stats_noaggregation_nbdaysdetailed_refferers(args)
-	stats_noaggregation_nbdaysdetailed_unique_refferers(args)
+	//stats_noaggregation_nbdaysdetailed_refferers(args)
+	//stats_noaggregation_nbdaysdetailed_unique_refferers(args)
 	stats_noaggregation_nbdaysdetailed_unique_refferers_noemptyorself(args)
 	stats_noaggregation_nbdaysdetailed_unique_refferers_noemptyorself_onlytld(args)
 	logger("finished the function to generate statistics")
