6b96e1fe6d898714ea62b004eb4998227a250b9f
Joris Vandenberg <joris.vandenberg@scarlet.be>
2023-03-17 06:20:05 -0400

    stat_perhour_referrers_unique_2xx_3xx/conf_stat_unique_PerHour_ReferringUrls

--- get_args.go
+++ get_args.go
@@ -327,7 +327,7 @@ func getargs() Args {
 		mystats = append(mystats, mystatconfig)
 	}
 
-	mystatconfig, err = argblock(cfg, "stat_perhour_referrers_unique_2xx_3xx", "t", outputs)
+	mystatconfig, err = argblock(cfg, "conf_stat_unique_PerHour_ReferringUrls", "t", outputs)
 	if err == nil {
 		mystats = append(mystats, mystatconfig)
 	}
