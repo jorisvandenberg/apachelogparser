a1327edc4771d20acf4ed2a3dbd69269e4a78490
Joris Vandenberg <joris.vandenberg@scarlet.be>
2023-03-17 06:34:44 -0400

    forgot to add the what to the filename

--- stats_generate.go
+++ stats_generate.go
@@ -65,7 +65,7 @@ func generatestats(args Args) {
 	/*
 		stat: raw 2xx and 3xx hits per hour over the last 31 (default) days
 		expecting 2 htmls:
-		raw_PerHour_hits.html
+		raw_PerHour_hits_table.html
 		noaggregation_nbdaysdetailed_raw_2xx_3xx_linegraph.html
 	*/
 	tableheaders = map[string]string{
