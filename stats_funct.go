661aeeb3ac1e3c7bb5f8d953b8442031c67da97f
Joris Vandenberg <joris.vandenberg@scarlet.be>
2023-03-28 06:18:08 -0400

    buggy version of multiple x week/month/year stats

--- stats_funct.go
+++ stats_funct.go
@@ -8,6 +8,18 @@ import (
 	"strings"
 )
 
+type compare_x_days_weeks_months_config_struct struct {
+What_hours_days_weeks_months string
+Number_of_days_weeks_months_compare int
+Number_of_days_weeks_months_compare_nbitems_inloop int
+Number_of_days_weeks_months_compare_legenda string
+Linegraph_compare_x_days_weeks_months_title string
+Linegraph_compare_x_days_weeks_months_description string
+Linegraph_compare_x_days_weeks_months_filename string
+Linegraph_compare_x_days_weeks_months_index_group string
+Linegraph_compare_x_days_weeks_months_index_order int
+}
+
 func genstats(args Args, string_for_log string, statname_from_conf string, querydb_key string, parameters []interface{}, tableheaders map[string]string, xaxisfields []int, valuefield int, legende string) bool {
 	//what_hours_days_weeks_months : usually hour or day
 	//number_of_days_weeks_months_compare : usually 4
@@ -39,19 +51,30 @@ func genstats(args Args, string_for_log string, statname_from_conf string, query
 		return false
 	}
 
-	var compare_x_days_weeks_months_parameters_string, what_hours_days_weeks_months, number_of_days_weeks_months_compare_legenda string
-	var number_of_days_weeks_months_compare, number_of_days_weeks_months_compare_nbitems_inloop int
+
 	var compare_x_days_weeks_months_parameters_parts []string
 	
+	//als mycurstat.Linegraph4weekinfo.Linegraph_compare_x_days_weeks_months_enabled == False what_hours_days_weeks_months,number_of_days_weeks_months_compare,number_of_days_weeks_months_compare_nbitems_inloop,number_of_days_weeks_months_compare_legenda vullen met standaard value, zoniet vullen met echte values en er dan door loopen
+	compare_x_days_weeks_months_config_struct_slice := []compare_x_days_weeks_months_config_struct{}
+	if !mycurstat.Linegraph4weekinfo.Linegraph_compare_x_days_weeks_months_enabled {
+		 compare_x_days_weeks_months_config_struct_slice = append(compare_x_days_weeks_months_config_struct_slice, compare_x_days_weeks_months_config_struct{"day",4,8,"week",mycurstat.Linegraph4weekinfo.Linegraph_compare_x_days_weeks_months_title, mycurstat.Linegraph4weekinfo.Linegraph_compare_x_days_weeks_months_description, mycurstat.Linegraph4weekinfo.Linegraph_compare_x_days_weeks_months_filename, mycurstat.Linegraph4weekinfo.Linegraph_compare_x_days_weeks_months_index_group, mycurstat.Linegraph4weekinfo.Linegraph_compare_x_days_weeks_months_index_order})
+	}
 	if mycurstat.Linegraph4weekinfo.Linegraph_compare_x_days_weeks_months_enabled {
-		compare_x_days_weeks_months_parameters_string = mycurstat.Linegraph4weekinfo.Linegraph_compare_x_days_weeks_months_parameters
-		compare_x_days_weeks_months_parameters_parts = strings.Split(compare_x_days_weeks_months_parameters_string, ",")
-		what_hours_days_weeks_months = compare_x_days_weeks_months_parameters_parts[0]
-		number_of_days_weeks_months_compare, _ = strconv.Atoi(compare_x_days_weeks_months_parameters_parts[1])
-		number_of_days_weeks_months_compare_nbitems_inloop, _ = strconv.Atoi(compare_x_days_weeks_months_parameters_parts[2])
-		number_of_days_weeks_months_compare_legenda = compare_x_days_weeks_months_parameters_parts[3]
+		linegraph_compare_x_days_weeks_months_parameters_slice = strings.Split(mycurstat.Linegraph4weekinfo.Linegraph_compare_x_days_weeks_months_parameters, ";")
+		linegraph_compare_x_days_weeks_months_title_slice = strings.Split(mycurstat.Linegraph4weekinfo.Linegraph_compare_x_days_weeks_months_title, ";")
+		Linegraph_compare_x_days_weeks_months_description_slice = strings.Split(mycurstat.Linegraph4weekinfo.Linegraph_compare_x_days_weeks_months_description, ";")
+		Linegraph_compare_x_days_weeks_months_filename_slice = strings.Split(mycurstat.Linegraph4weekinfo.Linegraph_compare_x_days_weeks_months_filename, ";")
+		Linegraph_compare_x_days_weeks_months_index_group_slice = strings.Split(mycurstat.Linegraph4weekinfo.Linegraph_compare_x_days_weeks_months_index_group, ";")
+		Linegraph_compare_x_days_weeks_months_index_order_slice = strings.Split(mycurstat.Linegraph4weekinfo.Linegraph_compare_x_days_weeks_months_index_order, ";")
+		indexnumber := 0
+		for _, linegraph_compare_x_days_weeks_months_parameters_slice_current := range linegraph_compare_x_days_weeks_months_parameters_slice {
+			compare_x_days_weeks_months_parameters_parts = strings.Split(linegraph_compare_x_days_weeks_months_parameters_slice_current, ",")
+			compare_x_days_weeks_months_config_struct_slice = append(compare_x_days_weeks_months_config_struct_slice, compare_x_days_weeks_months_config_struct{compare_x_days_weeks_months_parameters_parts[0],strconv.Atoi(compare_x_days_weeks_months_parameters_parts[1]),strconv.Atoi(compare_x_days_weeks_months_parameters_parts[2]),compare_x_days_weeks_months_parameters_parts[3],linegraph_compare_x_days_weeks_months_title_slice[indexnumber], Linegraph_compare_x_days_weeks_months_description_slice[indexnumber], Linegraph_compare_x_days_weeks_months_filename_slice[indexnumber], Linegraph_compare_x_days_weeks_months_index_group_slice[indexnumber], Linegraph_compare_x_days_weeks_months_index_order_slice[indexnumber] })
+			indexnumber += 1
+		}
 	}
 	
+	for _, current_x_days_weeks_months := range compare_x_days_weeks_months_config_struct_slice {
 	logger("start " + string_for_log)
 	myQuery := myquerydb[querydb_key].stmt
 	rows, err := myQuery.Query(parameters...)
@@ -74,7 +97,7 @@ func genstats(args Args, string_for_log string, statname_from_conf string, query
 
 	var XValues_linegraph []string
 	YValues_linegraph := make(map[string][]int)
-	XValues_linegraph_4weekcomp := []string{"today", what_hours_days_weeks_months +"-1", what_hours_days_weeks_months + "-2 ", what_hours_days_weeks_months +"-3", what_hours_days_weeks_months + "-4", what_hours_days_weeks_months + "-5", what_hours_days_weeks_months + "-6"}
+	XValues_linegraph_4weekcomp := []string{"today", current_x_days_weeks_months.What_hours_days_weeks_months +"-1", current_x_days_weeks_months.What_hours_days_weeks_months + "-2 ", current_x_days_weeks_months.What_hours_days_weeks_months +"-3", current_x_days_weeks_months.What_hours_days_weeks_months + "-4", current_x_days_weeks_months.What_hours_days_weeks_months + "-5", current_x_days_weeks_months.What_hours_days_weeks_months + "-6"}
 	YValues_linegraph_4weekcomp := make(map[string][]int)
 	weekcounter := 0
 	daycounter := 0
@@ -128,12 +151,12 @@ func genstats(args Args, string_for_log string, statname_from_conf string, query
 		XValues_linegraph = append(XValues_linegraph, titel)
 		YValues_linegraph[legende] = append(YValues_linegraph[legende], int(values[valuefield].(int64)))
 		daycounter++
-		if daycounter == number_of_days_weeks_months_compare_nbitems_inloop {
+		if daycounter == current_x_days_weeks_months.Number_of_days_weeks_months_compare_nbitems_inloop {
 			weekcounter++
 			daycounter = 1
 		}
-		if weekcounter < number_of_days_weeks_months_compare {
-			YValues_linegraph_4weekcomp[number_of_days_weeks_months_compare_legenda + " -"+strconv.Itoa(weekcounter)] = append(YValues_linegraph_4weekcomp[number_of_days_weeks_months_compare_legenda + " -"+strconv.Itoa(weekcounter)], int(values[valuefield].(int64)))
+		if weekcounter < current_x_days_weeks_months.Number_of_days_weeks_months_compare {
+			YValues_linegraph_4weekcomp[current_x_days_weeks_months.Number_of_days_weeks_months_compare_legenda + " -"+strconv.Itoa(weekcounter)] = append(YValues_linegraph_4weekcomp[current_x_days_weeks_months.Number_of_days_weeks_months_compare_legenda + " -"+strconv.Itoa(weekcounter)], int(values[valuefield].(int64)))
 		}
 
 	}
@@ -151,9 +174,10 @@ func genstats(args Args, string_for_log string, statname_from_conf string, query
 	}
 
 	if mycurstat.Linegraph4weekinfo.Linegraph_compare_x_days_weeks_months_enabled {
-		createlinegraph(XValues_linegraph_4weekcomp, YValues_linegraph_4weekcomp, mycurstat.Linegraph4weekinfo.Linegraph_compare_x_days_weeks_months_title, mycurstat.Linegraph4weekinfo.Linegraph_compare_x_days_weeks_months_description, args, mycurstat.Linegraph4weekinfo.Linegraph_compare_x_days_weeks_months_filename, mycurstat.Linegraph4weekinfo.Linegraph_compare_x_days_weeks_months_index_group, mycurstat.Linegraph4weekinfo.Linegraph_compare_x_days_weeks_months_index_order)
+		createlinegraph(XValues_linegraph_4weekcomp, YValues_linegraph_4weekcomp, current_x_days_weeks_months.Linegraph_compare_x_days_weeks_months_title, current_x_days_weeks_months.Linegraph_compare_x_days_weeks_months_description, args, current_x_days_weeks_months.Linegraph_compare_x_days_weeks_months_filename, current_x_days_weeks_months.Linegraph_compare_x_days_weeks_months_index_group, current_x_days_weeks_months.Linegraph_compare_x_days_weeks_months_index_order)
 	}
 
 	logger("stopped " + string_for_log)
+	}
 	return true
 }
